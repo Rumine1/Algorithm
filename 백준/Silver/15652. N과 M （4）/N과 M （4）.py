28322c9840d74e98216df7c89a8cf3602c680091
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-08-02 20:38:33 +0900

    [Silver III] Title: N과 M (4), Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+def calc(i, step):
+    global result
+    result.append(exam[i])
+    if step == M:
+        print(*result, sep=" ")
+        return
+
+    for j in range(N):
+        if result[-1] <= exam[j]:
+            calc(j, step+1)
+            result.pop()
+
+
+N, M = map(int, input().split())
+
+exam = [x for x in range(1, N+1)]
+result = []
+
+for i in range(N):
+    calc(i, 1)
+    result.clear()
