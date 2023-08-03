31d3a9d7d1bf390440547a3fb0cd0b4b63b9edb5
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-08-03 14:42:47 +0900

    [Silver III] Title: N과 M (8), Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+def calc(i, step):
+    global result
+    result.append(exam[i])
+    if step == M:
+        print(*result, sep=" ")
+        return
+
+    for j in range(N):
+        if result[-1] <= exam[j]:
+            calc(j, step+1)
+            result.pop()
+
+
+N, M = map(int, input().split())
+
+exam = sorted(list(set(list(map(int, input().split())))))
+result = []
+
+for i in range(N):
+    calc(i, 1)
+    result.clear()
