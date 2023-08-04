52f5d3062cb321921e3735b5d5a75bef09755870
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-08-04 21:17:24 +0900

    [Silver II] Title: N과 M (10), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+
+def calc(i, step):
+    global result
+    result.append(exam[i])
+    if step == M:
+        print(*result, sep=" ")
+        return
+    previous = 0
+    for j in range(len(exam)):
+        if previous != exam[j] and visit[j] == False and result[-1] <= exam[j]:
+            visit[j] = True
+            previous = exam[j]
+        # if result[-1] <= exam[j]:
+            calc(j, step+1)
+            result.pop()
+            visit[j] = False
+
+
+N, M = map(int, input().split())
+
+exam = sorted(list(map(int, input().split())))
+visit = [False for _ in range(N)]
+result = []
+
+for i in range(len(exam)):
+    if exam[i] != exam[i-1]:
+        visit[i] = True
+        calc(i, 1)
+        visit[i] = False
+        result.clear()
+    elif i == 0:
+        visit[i] = True
+        calc(i, 1)
+        visit[i] = False
+        result.clear()
