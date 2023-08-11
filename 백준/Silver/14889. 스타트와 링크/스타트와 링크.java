7f14db84ce94dc95ea508b096ad1f0d00c75a0bd
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-08-11 11:41:13 +0900

    [Silver II] Title: 스타트와 링크, Time: 372 ms, Memory: 18104 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private int N;
+	private int[][] S;
+	private int minValue;
+	private boolean[] visited;
+
+	public void sumStatus() {
+		int startTeam = 0;
+		int linkTeam = 0;
+
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (visited[j] == true && visited[i] == true) {
+					startTeam += S[i][j];
+					startTeam += S[j][i];
+				} else if (visited[j] == false && visited[i] == false) {
+					linkTeam += S[i][j];
+					linkTeam += S[j][i];
+				}
+			}
+		}
+
+		minValue = Math.min(minValue, Math.abs(startTeam - linkTeam));
+
+		if (minValue == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
+	}
+
+	public void divideTeam(int idx, int start) {
+		if (idx == N / 2) {
+			sumStatus();
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				divideTeam(idx + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		S = new int[N][N];
+		minValue = Integer.MAX_VALUE;
+		visited = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				S[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		boolean[] visited = new boolean[N];
+		for (int i = 0; i < N; i++) {
+			visited[i] = false;
+		}
+
+		divideTeam(0, 0);
+
+		System.out.println(minValue);
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+
+}
