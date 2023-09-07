3ef1d36e976cd86c91f9a9c672eee77d929aa933
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-09-07 13:41:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 236 ms, Memory: 21192 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    private int N,M;
+    private void solution()  {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        if(N>=M){
+            System.out.println(N-M);
+        }else{
+            System.out.println(Bfs());
+        }
+    }
+
+    private int Bfs(){
+        int[] distance = new int[100001];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(N);
+        distance[N] = 1;
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+            for(int i=0;i<3;i++){
+                int nx;
+                if(i==0){
+                    nx = x-1;
+                } else if (i == 1) {
+                    nx = x+1;
+                }else{
+                    nx = x*2;
+                }
+
+                if(nx==M){
+                    return distance[x];
+                }
+
+                if(nx>=0 && nx<100001 && distance[nx]==0){
+                    distance[nx] = distance[x] + 1;
+                    queue.add(nx);
+                }
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+}
+
