5675d2d4b7e6701fb4551e62ce6b9cbe4dfbb047
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-09-16 17:29:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1416 ms, Memory: 79700 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    ArrayList<Integer>[] list;
+    int[] answer;
+    boolean[] visited;
+    public void solution()throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        answer = new int[N+1];
+        list = new ArrayList[N+1];
+        visited = new boolean[N+1];
+
+        for(int i=0;i<=N;i++){
+            list[i] = new ArrayList<Integer>();
+        }
+
+        for(int i = 0;i<N-1;i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            list[A].add(B);
+            list[B].add(A);
+        }
+
+        dfs(1, 0);
+
+        for(int i=2;i<=N;i++){
+            System.out.println(answer[i]);
+        }
+    }
+
+    public void dfs(int current, int previous){
+        visited[current] = true;
+        answer[current] = previous;
+        for(int temp:list[current]){
+            if(temp!=previous && !visited[temp]){
+                dfs(temp, current);
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        new Main().solution();
+    }
+}
