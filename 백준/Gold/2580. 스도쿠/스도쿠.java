24abc81ac7b82367333e835eca44875e2be32c73
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-10-12 11:03:17 +0900

    [Gold IV] Title: 스도쿠, Time: 460 ms, Memory: 25000 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,93 +1,81 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static int[][] arr = new int[9][9];
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		for (int i = 0; i < 9; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
- 
-		sudoku(0, 0);
- 
-	}
- 
-	public static void sudoku(int row, int col) {
- 
-		
-		if (col == 9) {
-			sudoku(row + 1, 0);
-			return;
-		}
- 
-		
-		if (row == 9) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					sb.append(arr[i][j]).append(' ');
-				}
-				sb.append('\n');
-			}
-			System.out.println(sb);
-			
-			System.exit(0);
-		}
- 
-		
-		if (arr[row][col] == 0) {
-			for (int i = 1; i <= 9; i++) {
-				
-				if (possibility(row, col, i)) {
-					arr[row][col] = i;
-					sudoku(row, col + 1);
-				}
-			}
-			arr[row][col] = 0;
-			return;
-		}
- 
-		sudoku(row, col + 1);
- 
-	}
- 
-	public static boolean possibility(int row, int col, int value) {
- 
-		for (int i = 0; i < 9; i++) {
-			if (arr[row][i] == value) {
-				return false;
-			}
-		}
- 
-		
-		for (int i = 0; i < 9; i++) {
-			if (arr[i][col] == value) {
-				return false;
-			}
-		}
- 
-		int set_row = (row / 3) * 3; 
-		int set_col = (col / 3) * 3; 
- 
-		for (int i = set_row; i < set_row + 3; i++) {
-			for (int j = set_col; j < set_col + 3; j++) {
-				if (arr[i][j] == value) {
-					return false;
-				}
-			}
-		}
- 
-		return true; 
-	}
- 
+import java.io.*;
+import java.util.*;
+
+class Main{
+    private int[][] map;
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        map = new int[9][9];
+        for(int i=0;i<9;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<9;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        search(0,0);
+    }
+
+    public void search(int y, int x){
+        if(x==9){
+            search(y+1,0);
+            return;
+        }
+
+        if(y==9){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<9;i++){
+                for(int j=0;j<9;j++){
+                    sb.append(map[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb);
+            System.exit(0);
+        }
+
+        if(map[y][x]==0){
+            for(int i=1;i<=9;i++){
+                if(check(y,x,i)){
+                    map[y][x] = i;
+                    search(y,x+1);
+                }
+            }
+            map[y][x] = 0;
+            return;
+        }
+
+        search(y,x+1);
+    }
+
+    public boolean check(int y, int x, int value){
+        for(int i=0;i<9;i++){
+            if(map[i][x]==value){
+                return false;
+            }
+
+            if(map[y][i]==value){
+                return false;
+            }
+        }
+
+        int startY = y/3 *3;
+        int startX = x/3 *3;
+
+        for(int i=startY;i<startY+3;i++){
+            for(int j=startX;j<startX+3;j++){
+                if(map[i][j]==value){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
 }
