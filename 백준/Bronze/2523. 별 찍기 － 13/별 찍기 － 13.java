8b39bd3a9bc7bed58ce9e99301d9a6c0a3dec30b
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-11-04 23:29:39 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class Main {
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<=i;j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for(int i=N-1;i>0;i--){
+            for(int j=i;j>0;j--){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+}
