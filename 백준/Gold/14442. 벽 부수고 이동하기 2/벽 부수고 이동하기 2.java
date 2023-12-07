9857dcd1798a0fd595d97be36ab4b162cb51297a
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-12-07 11:02:38 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1764 ms, Memory: 370568 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    private int N, M, K;
+    private boolean[][] map;
+    private boolean[][][] visited;
+    private int[] dirX = {1, 0, -1, 0};
+    private int[] dirY = {0, 1, 0, -1};
+    public void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new boolean[N][M];
+        visited = new boolean[N][M][K+1];
+
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            for(int j=0;j<M;j++){
+                map[i][j] = str.charAt(j)=='0'?true:false;
+            }
+        }
+
+        if(N==1&&M==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(bfs(new Pos(0,0, 1,0)));
+
+    }
+
+    public int bfs(Pos first){
+        Queue<Pos> queue = new LinkedList<>();
+        visited[first.x][first.y][first.crush] = true;
+        queue.add(first);
+
+        while(!queue.isEmpty()){
+            Pos cur = queue.poll();
+
+            for(int i=0;i<4;i++){
+                int nx = cur.x + dirX[i];
+                int ny = cur.y + dirY[i];
+                int count = cur.count;
+                int crush = cur.crush;
+
+                if(nx<0||ny<0||nx>=N||ny>=M){continue;}
+                if(nx == N-1 && ny == M-1){
+                    return count+1;
+                }
+                if(!map[nx][ny]){
+                    if(crush==K){continue;}
+
+                    crush += 1;
+
+                }
+                if(visited[nx][ny][crush]){continue;}
+
+                visited[nx][ny][crush] = true;
+
+                queue.add(new Pos(nx, ny, count+1, crush));
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+}
+
+class Pos{
+    public Pos(int x, int y, int count, int crush){
+        this.x = x;
+        this.y = y;
+        this.count = count;
+        this.crush = crush;
+    }
+
+    protected int x, y, count;
+    protected int crush;
+}
