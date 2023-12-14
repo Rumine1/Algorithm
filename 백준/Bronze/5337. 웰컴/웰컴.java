d30c4254e219311bc3fc706f9b7faa23ccf10433
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-12-14 22:24:05 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+
+    public void solution(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+
+
+        System.out.println(sb);
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+}
