6df12abab5f210cf800852a90d8f45ceb0e21af0
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-12-18 00:04:29 +0900

    [Bronze IV] Title: 방학 숙제, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int L = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        
+        double max = 0;
+        max = Math.max((double)A/(double)C, (double)B / (double)D);
+        max = Math.ceil(max);
+        L -= (int)max;
+        
+        System.out.print(L);
+    }
+
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+}
