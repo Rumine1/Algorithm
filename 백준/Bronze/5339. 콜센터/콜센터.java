692c353e18aa3aef482a536009d28a2201641a87
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-12-22 23:37:43 +0900

    [Bronze V] Title: 콜센터, Time: 112 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public void solution() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\").append("\n");
+        sb.append("    ( oo|").append("\n");
+        sb.append("    _\\=/_").append("\n");
+        sb.append("   /  _  \\").append("\n");
+        sb.append("  //|/.\\|\\\\").append("\n");
+        sb.append(" ||  \\ /  ||").append("\n");
+        sb.append("============").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |").append("\n");
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+}
