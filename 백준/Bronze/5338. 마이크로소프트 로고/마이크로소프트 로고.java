f22102e656e81e65eb6f9161e5b5bf6be7df4fab
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2023-12-24 19:12:32 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public void solution() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-;;-._|").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-''-._|").append("\n");
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+}
