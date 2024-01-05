ea9714a91f5fe096055842e294f616bec9df55ed
Rumine1 <125424312+Rumine1@users.noreply.github.com>
2024-01-05 18:34:38 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken("/"));
+        int D = Integer.parseInt(st.nextToken("/"));
+        int A = Integer.parseInt(st.nextToken("/"));
+
+        if(K+A<D || D==0){
+            System.out.println("hasu");
+        }else{
+            System.out.println("gosu");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
+
